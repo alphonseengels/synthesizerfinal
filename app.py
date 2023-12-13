5eeb50b8919a4730374c093b226591bab286204e
Alphonse Engels <alphonse.engels24@ncf.edu>
2023-12-12 22:23:40 -0500

    update 12/12/23 10:23

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-#notes
-'''
-Implement high pass, low pass, and band pass filter using
-a fast fourier transform (FFT)
-this is possibly built into numpy
-'''
+'''I tried to make the oscillators
+overlap each other perfectly... but could not achieve this
 
-
-'''
-figure out *how* a LFO modulates things like pitch,
-filter, etc...
-'''
-
-'''
-allow the play function to play multiple oscillators at the same time...
-
-how?
-way 1: somehow add oscillators together and pass the addition to the function? 
-way 2: pass multiple oscillators to the function?
-'''
+another known bug: problems setting oscillator 2 and 3 to none'''
 
 
